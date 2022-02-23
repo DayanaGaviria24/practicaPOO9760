5ba28dad2f1e458c65da1e1218f5d1bac7fbc55a
DayanaGaviria24 <kardayan5@gmail.com>
2022-02-23 17:11:10 -0500

    Se realiza un cambio

---
+++ practicaPOO9760/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace practicaPOO9760
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cambio");
+        }
+    }
+}
